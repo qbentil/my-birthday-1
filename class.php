064802442a57afeb45f65df1877a6d6f86c2c128
qbentil <Bentilshadrack72@gmail.com>
2021-08-24 01:13:08 +0000

    Adding Snippet Class

---
+++ class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* 
+*/
+class Snippet
+{
+    // function __construct()
+    // {
+    //     require_once "./addwish.php";
+    // }
+
+    public function get_formbook()
+    {
+        require_once "./addwish.php";
+    }
+}
+
+// $db = new Database();
+// echo $db->connect();
+
+?>
