3d0a5671271e383c367d9be94a9d05ac12c42f36
qbentil <Bentilshadrack72@gmail.com>
2021-08-24 11:49:12 +0000

    Sanitize Home Page

--- index.php
+++ index.php
@@ -14,51 +14,10 @@
 
     ?>
     <footer> <a href="javascript:;" class="trigger">Add your message to My Wishes Book💖!</a></footer>
-    
-    <!-- <div class="card">
-        <div class="back"></div>
-        <div class="front">
-        <div class="imgset">
-            <img width="100%" src="./img/front.jpg">
-            </div>
-        </div>
-        <div class="text-container">
-        <p id="head">Happy Birthday Bby❤💖!</p>
-        <p>I hope your special day will bring you lots of happiness, love, and fun. You deserve them a lot. Enjoy your day for me!</p>
-        <p>Have a memorable day!🥰</p>
-        </div>
     </div> -->
     <?php
         $snippet->get_card("Happy Birthday Bentil","I hope your special day will bring you lots of happiness, love, and fun. You deserve them a lot. Enjoy your day for me!", "John Doe", "happy.jpg" )
     ?>
-    <div class="card">
-        <div class="back"></div>
-        <div class="front">
-        <div class="imgset">
-            <img width="100%" src="./img/happy.jpg">
-            </div>
-        </div>
-        <div class="text-container">
-        <p id="head">Happy Birthday Minash!</p>
-        <p>My wish for you on this special day of yours is that life brings you everything that puts a smile on your face. You are such a wonderful woman, and I love you more than I can ever say in words. Happy birthday Bby❤💖. 
-        </p>
-        <p>I Love you💖!</p>
-        </div>
-    </div>
-
-    <!-- <div class="card">
-        <div class="back"></div>
-        <div class="front">
-        <div class="imgset">
-            <img width="100%" src="./img/happy.jpg">
-            </div>
-        </div>
-        <div class="text-container">
-        <p id="head">Happy Birthday Minash!</p>
-        <p>I hope your special day will bring you lots of happiness, love, and fun. You deserve them a lot. Enjoy!</p>
-        <p>Hope your day goes great!</p>
-        </div>
-    </div> -->
 
   <?php echo $snippet->get_formbook() ?>
   <footer>&copy; <a href="http://qbentil.netlify.com" target="_blank" rel="noopener noreferrer">Themanbentil</a> Made it💖!</footer>
