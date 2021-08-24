e9a70e085906705a61f14293dda4789805376247
qbentil <Bentilshadrack72@gmail.com>
2021-08-24 01:13:19 +0000

    Form Book Update

--- addwish.php
+++ addwish.php
@@ -6,14 +6,17 @@
               <div class="ajax-message"></div>
             <form id="wish-book" method="POST" autocomplete="off">
 
-              <label for="fname">Your name</label>
-              <input type="text" id="name"  class="form-group" name="name" placeholder="Your name..">
+              <!-- <label for="fname">Your name</label> -->
+              <input type="text" id="name"  class="form-group" name="name" placeholder="Enter your name">
 
-              <label for="lname">Hyped Wish</label>
-              <input type="text" id="subject" class="form-group" name="subject" placeholder="Happy Birthday Mina">
+              <!-- <label for="fname">Your Phone</label> -->
+              <input type="tel" id="phone"  class="form-group" name="phone"  placeholder="Enter your phone number">
 
-              <label for="subject">Wish Message</label>
-              <textarea id="subject" class="form-group" name="message" placeholder="Write something for Minash.." style="height:200px"></textarea>
+              <!-- <label for="lname">Hyped Wish</label> -->
+              <input type="text" id="subject" class="form-group" name="subject" placeholder="Message Title: Glorous Birthday to you Bentil">
+
+              <!-- <label for="subject">Wish Message</label> -->
+              <textarea id="subject" class="form-group" name="message" placeholder="Write something for Bentil💻🥰" style="height:200px"></textarea>
 
               <input type="submit" id="send" value="Submit">
 
