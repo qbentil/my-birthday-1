c5f1c990502a1de5cbc2535b23463e9d2dc3a641
qbentil <Bentilshadrack72@gmail.com>
2021-08-25 13:27:10 +0000

    Updating From Style

--- routes/addwish.php
+++ routes/addwish.php
@@ -1,7 +1,7 @@
 <div class="modal">
         <div class="modal-content">
             <span class="close-button">&times;</span>
-            <h1>Wishes Book</h1>
+            <h2>Creating a Wish Book</h2>
             <div class="container">
               <div class="ajax-message"></div>
             <form id="wish-book" method="POST" autocomplete="off">
@@ -16,7 +16,7 @@
               <input type="text" id="subject" class="form-group" name="subject" placeholder="Message Title: Glorous Birthday to you Bentil">
 
               <label for="subject"><Small><b>Maximum: 45 Words:  </b><i> <span id="lcount">45</span> Words remaining</i></Small></label>
-              <textarea id="message" class="form-group" name="message" placeholder="Write something for Bentil💻🥰 [Max: 225 Letters]" style="height:200px" maxlength="225"></textarea>
+              <textarea id="message" class="form-group" name="message" placeholder="Write something for Bentil💻🥰 [Max: 225 Letters]" style="height:160px" maxlength="225"></textarea>
 
               <input type="submit" id="send" value="Submit">
 
