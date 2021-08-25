01447fd7f646d712d3cf043d0eb651c455c047c7
qbentil <Bentilshadrack72@gmail.com>
2021-08-25 00:15:37 +0000

    Services and Proccessor Files Update

--- routes/services.php
+++ routes/services.php
@@ -1,5 +1,13 @@
 <?php
 if(isset($_POST['action']) && $_POST['action'] == 'send_wish')
 {
-    echo "From PHP";
+    require_once "processor.php";
+    $proccessor = new Processor();
+    $add = $proccessor->send_wish($_POST['name'], $_POST['phone'], $_POST['subject'], $_POST['message']);
+    if($add)
+    {
+        echo json_encode(["status" => 1, "message" => "Thank you for your wonderful words🥰"]);
+    }else{
+        echo json_encode(["status" => 0, "message" => "Sorry, Each person is entitle to one wish book. Thank you🥰"]); 
+    }
 }
