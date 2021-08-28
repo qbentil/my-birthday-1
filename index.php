68b7d67614b138165b1c58d1e33426e69ba8d7f7
Bentil Shadrack <bentilshadrack72@gmail.com>
2021-08-28 08:14:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Happy Birthday | QUadjo Bentil</title>
+    <meta name="Author" content="Shadrack Bentil">
+    <title>Happy Birthday | Quadjo Bentil</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <?php
         require "./routes/class.php";
         $snippet = new Snippet();
-
     ?>
     <footer> <a href="javascript:;" class="trigger">Add your message to My Wishes Book &#x2295;</a></footer>
     </div>
@@ -22,7 +22,7 @@
   <?php echo $snippet->get_formbook() ?>
   <footer>&copy; <a href="http://qbentil.netlify.com" target="_blank" rel="noopener noreferrer">Themanbentil</a> Made it💖!</footer>
 
-  <!-- jquery -->
+  <!-- Custom and Lib JS -->
   <script src="./js/jquery.js"></script>
   <script src="./js/app.js"></script>
   </body>
